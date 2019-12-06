74dd63b82e888b7af8f50117527b3dc7f18bcf79
sherotree <wytreeshero@163.com>
2019-12-06 15:35:50 +0800

    focus:bug

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,13 +1,11 @@
 import React, { useState } from 'react'
+import './Calculator.css'
 
 export function Calculator() {
-  const [sum, setSum] = useState(0)
   const [row, setRow] = useState([
     { name: '示例1', price: 25, quantity: 3, total: 75 },
     { name: '示例2', price: 32, quantity: 4, total: 128 },
   ])
-
-  console.log(row, 22)
 
   function handleName(e, index) {
     row[index].name = e.target.value
@@ -22,19 +20,17 @@ export function Calculator() {
   function handleQuantity(e, index) {
     row[index].quantity = e.target.value
     row[index].total = row[index].quantity * row[index].price
-    setSum(total(row))
     setRow([...row])
   }
 
-  function handleTotal() {
-    setRow([...row])
+  function toSum(row) {
+    let sum = 0
+    row.forEach(item => {
+      sum += item.total
+    })
+    return sum
   }
 
-  function total(row) {
-    for (let i = 0; i < row.length; i++) {
-      setSum(sum + row.total)
-    }
-  }
   function handleAdd() {
     row.push({ name: '', price: '', quantity: '', total: '' })
     setRow([...row])
@@ -42,52 +38,52 @@ export function Calculator() {
 
   function Table() {
     return (
-      <div>
-        <div>
-          <span>产品</span>
-          <span>价格</span>
-          <span>数量</span>
-          <span>合计</span>
+      <div className="table">
+        <div className="tableTitle">
+          <span className="title">产品</span>
+          <span className="title">价格</span>
+          <span className="title">数量</span>
+          <span className="title">合计</span>
         </div>
         <div>
           {row.map((item, index) => {
             return (
-              <div key={index}>
+              <div className="row" key={index}>
                 <input
                   placeholder="产品.."
                   value={item.name}
                   onChange={e => handleName(e, index)}
+                  className="cell"
                 />
                 <input
                   placeholder="价格.."
                   value={item.price}
                   onChange={e => handlePrice(e, index)}
+                  className="cell"
                 />
                 <input
                   placeholder="数量.."
                   value={item.quantity}
                   onChange={e => handleQuantity(e, index)}
+                  className="cell"
                 />
-                <input
-                  placeholder="合计.."
-                  value={item.total}
-                  onChange={handleTotal}
-                />
+
+                <span className="cell">{item.total}</span>
               </div>
             )
           })}
+        </div>
+        <div className="sum">
+          <span>总计:</span>
+          <span>{toSum(row)}</span>
         </div>
       </div>
     )
   }
   return (
-    <div>
+    <div className="wrapper">
       <h1>动态收银表</h1>
       <Table />
-      <div>
-        <span>总计:</span>
-        <span>{sum}</span>
-      </div>
       <div>
         <button onClick={handleAdd}>+</button>
         <span>Add Product</span>
